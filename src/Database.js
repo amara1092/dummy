12c2f0f084976507c01fba8095ee70ac2c1b5815
amara1092 <amara1092@gmail.com>
2021-02-23 14:41:22 +0000

    Async local storage applied

---
+++ src/Database.js
@@ -0,0 +1,60 @@
+import SQLite from "react-native-sqlite-storage";
+SQLite.DEBUG(true);
+SQLite.enablePromise(true);
+
+const database_name = "Timesheetapp.db";
+const database_version = "1.0";
+const database_displayname = "SQLite React Offline Database";
+const database_size = 200000;
+
+export default class Database {
+    initDB() {
+        let db;
+        return new Promise((resolve) => {
+          console.log("Plugin integrity check ...");
+          SQLite.echoTest()
+            .then(() => {
+              console.log("Integrity check passed ...");
+              console.log("Opening database ...");
+              SQLite.openDatabase(
+                database_id_timesheet,
+                database_user_id,
+                database_eow,
+                database_date,
+                database_projNum,
+                database_comment,
+                database_arriveTime,
+                database_departTime,
+                database_totalHrs,
+                database_siteID,
+                database_isTravel,
+                database_timeInserted
+              )
+                .then(DB => {
+                  db = DB;
+                  console.log("Database OPEN");
+                  db.executeSql('SELECT 1 FROM Product LIMIT 1').then(() => {
+                      console.log("Database is ready ... executing query ...");
+                  }).catch((error) =>{
+                      console.log("Received error: ", error);
+                      console.log("Database not yet ready ... populating data");
+                      db.transaction((tx) => {
+                          tx.executeSql('CREATE TABLE IF NOT EXISTS Product (prodId, prodName, prodDesc, prodImage, prodPrice)');
+                      }).then(() => {
+                          console.log("Table created successfully");
+                      }).catch(error => {
+                          console.log(error);
+                      });
+                  });
+                  resolve(db);
+                })
+                .catch(error => {
+                  console.log(error);
+                });
+            })
+            .catch(error => {
+              console.log("echoTest failed - plugin not functional");
+            });
+          });
+      };
+}
